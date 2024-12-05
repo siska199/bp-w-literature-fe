af9d5522e6e05287c69d686ca5d92c5026c03bda
siska199 <siska.apriana.1703226@students.um.ac.id>
2024-12-05 07:59:23 +0700

    update: script.sh

--- script.sh
+++ script.sh
@@ -1,3 +1,3 @@
 docker build -t literature-fe/v1 .
 
-docker run --name c-literature-fe -V ${PWD}:/kyuuchan199/literature-fe -p 3001:3001 literature-fe/v1
+docker run --name c-literature-fe --volume=${PWD}:/kyuuchan199/literature-fe -p 3001:3001 literature-fe/v1
